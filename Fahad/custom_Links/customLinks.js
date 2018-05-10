c810a03b0339c9cbdb30a1c70228ef6fd0ea4682
Frédéric Devernay <devernay@users.noreply.github.com>
2018-05-10 19:09:09 +0200

    Fahad: update Natron version

--- Fahad/custom_Links/customLinks.js
+++ Fahad/custom_Links/customLinks.js
@@ -1,7 +1,7 @@
 //Varaibles
 /* version number: changing this number
 will update all version centric variables */
-var versionnum = "2.3.10"
+var versionnum = "2.3.11"
 
 //previous version link
 var preversion = "https://sourceforge.net/projects/natron/files/"
