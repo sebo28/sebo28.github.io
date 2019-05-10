3d146df40c7f7a7fb816444585ebbe2889de2d4b
sebo28 <sebo_2845@hotmail.com>
2019-05-10 03:04:39 +0300

    Update customLinks.js

--- custom_Links/customLinks.js
+++ custom_Links/customLinks.js
@@ -7,7 +7,7 @@ var versionnum = "2.3.14"
 var preversion = "https://sourceforge.net/projects/natron/files/"
 
 // Git hub Main Repo Page Link
-var mainRepoLink = "http://sebo28github.github.io";
+var mainRepoLink = "https://github.com/sebo28GitHub/sebo28";
 // Community plugin links
 var communityplug = "https://github.com/NatronGitHub/natron-plugins";
 
